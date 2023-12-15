d4f37246e5b135f93692038bff26aa8526879eb9
sanjay kanth <Sanjaylavel@gmail.com>
2023-12-15 22:37:54 +0530

    changed css

--- user_home.php
+++ user_home.php
@@ -26,35 +26,35 @@
 
         // Close the dropdown menu if the user clicks outside of it
         window.onclick = function(event) {{
-            if (event.target.id != 'prjTitle' && event.target.id != 'prjListDD') 
-            {
-                document.getElementById('project-dropdown').style.display = "none";
-            }
-            else
+            if (event.target.id == 'prjTitle' || event.target.id == 'prjListDD') 
                 toggleProjectList();
+            else
+                document.getElementById('project-dropdown').style.display = "none";
         }}
 
     </script>
 
-    <div class="main-content">    
-        <div class="dashboard">
-            <div class="dashboard-header">
-                <div>                    
-                    <span id="prjTitle" class="db-prj-title">Current Project</span><i class="fa fa-thin fa-chevron-down" id="prjListDD"></i>
-                    <div id="project-dropdown" class="project-list" >
-                        <a href="#">Project Other</a><hr>
-                        <a href="#">Yet another project</a><hr>
-                        <a href="#">YOLO</a>
-                    </div>
+    <div class="dashboard">
+        <div class="dashboard-header">
+            <div class="prj-title-holder">                    
+                <span id="prjTitle" class="db-prj-title">Current Project</span><i class="fa fa-thin fa-chevron-down" id="prjListDD"></i>
+                <div id="project-dropdown" class="project-list" >
+                    <a href="#">Project Other</a><hr>
+                    <a href="#">Yet another project</a><hr>
+                    <a href="#">YOLO</a>
                 </div>
-                
-                <span class="db-header-end">Status: In progress</span>
-                <hr class="db-vl">
-                <span class="db-header-item">Start Date: 00-00-0000</span>
             </div>
-            <hr class="dashboard-hr">
             
+            <span class="db-header-end">Status: In progress</span>
+            <hr class="db-vl">
+            <span class="db-header-item">Start Date: 00-00-0000</span>
         </div>
+        <hr class="dashboard-hr">
+        
+        <div class="db-sidebar">
+
+        </div>
+        
     </div>
 
     <?php
